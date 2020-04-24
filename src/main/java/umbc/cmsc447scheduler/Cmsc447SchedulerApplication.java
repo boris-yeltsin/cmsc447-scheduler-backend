a887357269458948b3fc9e535a91a1d862dd14c1
Adam Long <along8@umbc.edu>
2020-04-24 15:51:42 -0400

    initial commit

---
+++ src/main/java/umbc/cmsc447scheduler/Cmsc447SchedulerApplication.java
@@ -0,0 +1,13 @@
+package umbc.cmsc447scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmsc447SchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmsc447SchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umbc/cmsc447scheduler/Cmsc447SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package umbc.cmsc447scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmsc447SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
